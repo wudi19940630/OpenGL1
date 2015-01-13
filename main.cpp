e6b7a0face6f93a52ba25460e9f75b9f7caaa702
wudi19940630 <wudi19940630@yahoo.com>
2015-01-13 16:04:55 +0000

    added window

--- main.cpp
+++ main.cpp
@@ -3,5 +3,22 @@
 
 int main()
 {
- std::cout<<"hello world\n";
+ if(SDL_Init(SDL_INIT_VIDEO))
+ {
+     std::cerr<<"error int SDL\n";
+     exit(EXIT_FAILURE);
+ }
+ SDL_Rect screenSize;
+ SDL_GetDisplayBounds(0,&screenSize);
+
+ SDL_Window *window = SDL_CreateWindow (
+             "OpenGL DEMO",
+             SDL_WINDOWPOS_CENTERED,
+             SDL_WINDOWPOS_CENTERED,
+             screenSize.w/2,
+             screenSize.h/2,
+             SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+             );
+ SDL_Delay(10000);
 }
+
